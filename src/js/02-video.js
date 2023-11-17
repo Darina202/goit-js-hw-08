4a7136215ff2e170253b9ec45ee68164661b6f22
Darina202 <138057834+Darina202@users.noreply.github.com>
2023-11-17 23:07:00 +0200

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (e) {
+    localStorage.setItem(LOCAL_STORAGE_KEY, e.seconds);
+  }, 1000)
+);
+
+// function timeGet(e) {
+//   localStorage.setItem(LOCAL_STORAGE_KEY, e.seconds);
+// }
+// player.on('timeupdate', throttle(timeGet, 1000));
+
+const time = localStorage.getItem(LOCAL_STORAGE_KEY);
+player
+  .setCurrentTime(time)
+  // працює і без промісів, але краще залишити щоб не було помилок в консолі
+  .then(function () {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,57 @@
+import throttle from 'lodash.throttle';
 
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+const feedbackForm = document.querySelector('.feedback-form');
+const { email, message } = feedbackForm.elements;
+
+feedbackForm.addEventListener('input', throttle(inputToLS, 500));
+
+feedbackForm.addEventListener('submit', e => {
+  e.preventDefault();
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  console.log(load('obj'));
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+  feedbackForm.reset();
+});
+
+function inputToLS(event) {
+  //   let email;
+  //   let message;
+  //   if (event.target.name === 'email') {
+  //     email = ev.value;
+  //     console.log(email);
+  //   }
+  //   if (event.target.name === 'message') {
+  //     message = event.target.value;
+  //     console.log(message);
+  //   }
+  //   const message = event.target.value;
+
+  const obj = {
+    email: email.value.trim() || '',
+    message: message.value.trim() || '',
+  };
+
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(obj));
+}
+let data;
+function load() {
+  data = localStorage.getItem(LOCAL_STORAGE_KEY) || '';
+  try {
+    return JSON.parse(data);
+  } catch {
+    return data;
+  }
+}
+
+function loadFromLS() {
+  if (load('obj') === '') {
+    return;
+  }
+  const obj = load('obj');
+  email.value = obj.email;
+  message.value = obj.message;
+}
+loadFromLS();
